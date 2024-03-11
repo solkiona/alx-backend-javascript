99ccd11dcebf26b1364e8e7b217c44b7d2d66865
SOLKIONA <solkiona@gmail.com>
2024-03-11 14:25:12 +0100

    employees object

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,8 +1,6 @@
 export default function createEmployeesObject(departmentName, employees) {
 
     return {
-        [departmentName]: [
-            `${employees}`
-        ]
+        [departmentName]:employees,
    }
 }
