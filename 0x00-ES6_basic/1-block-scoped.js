53e12360e6403a3057fb54018609d9409be67879
SOLKIONA <solkiona@gmail.com>
2024-03-11 10:46:09 +0100

    Hoist removal

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,13 +3,9 @@ export default function taskBlock(trueOrFalse) {
     const task2 = true;
   
     if (trueOrFalse) {
-      const task = true;
-      const task2 = false;
+      const task = true; //not overridden during execution
+      const task2 = false; // not overriden during execution
     }
   
     return [task, task2];
   }
-
-
-  console.log(taskBlock(true));
-  console.log(taskBlock(false));
