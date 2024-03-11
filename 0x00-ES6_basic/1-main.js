6d3d0692e6dc3d4e432f9e59a98a73d3c0330f22
SOLKIONA <solkiona@gmail.com>
2024-03-11 11:13:05 +0100

    defualt parameters

--- 0x00-ES6_basic/1-main.js
+++ 0x00-ES6_basic/1-main.js
@@ -1,5 +1,4 @@
-import getNeighborhoodsList from './2-arrow.js';
+import taskBlock from './1-block-scoped.js';
 
-const neighborhoodsList = new getNeighborhoodsList();
-const res = neighborhoodsList.addNeighborhood('Noe Valley');
-console.log(res);
+console.log(taskBlock(true));
+console.log(taskBlock(false));
