5b0fe52434623bc8a002702d7408fefa19a60256
SOLKIONA <solkiona@gmail.com>
2024-03-11 10:33:51 +0100

    solved the hoist issue

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,15 @@
+export default function taskBlock(trueOrFalse) {
+    var task = false;
+    var task2 = true;
+  
+    if (trueOrFalse) {
+      const task = true;
+      const task2 = false;
+    }
+  
+    return [task, task2];
+  }
+
+
+  console.log(taskBlock(true));
+  console.log(taskBlock(false));
