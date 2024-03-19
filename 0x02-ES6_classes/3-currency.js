4ce3863eb213e066e9eeb9dc0c155619f0ebe2a9
SOLKIONA <solkiona@gmail.com>
2024-03-19 16:58:15 +0100

    holbertonn class

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -4,6 +4,22 @@ export default class Currency {
         this._name = name;
     }
 
+    get code() {
+        return this._code;
+    }
+    
+    set code(value) {
+        this._code = value;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        this._name = value;
+    }
+    
     displayFullCurrency() {
         return `${this._name} (${this._code})`;
     }
