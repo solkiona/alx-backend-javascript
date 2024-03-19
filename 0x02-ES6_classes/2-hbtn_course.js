262878153fb87d14512a4528fc7ce1989186ef50
SOLKIONA <solkiona@gmail.com>
2024-03-19 16:41:01 +0100

    holberton course

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -8,28 +8,34 @@ export default class HolbertonCourse{
     get name(){
         return this._name;
     }
-    set name(name) {
+    set name(value) {
 
-        if (typeof name !== 'string'){
+        if (typeof value !== 'string'){
             throw TypeError("Name must be a string");
         }
-        this._name = name;
+        this._name = value;
     }
 
     get length(){
         return this._length;
     }
-    set length(length) {
-        if (typeof length!== 'number'){
+    set length(value) {
+        if (typeof value !== 'number'){
             throw TypeError("Length must be a number");
         }
-        this._length = length;
+        this._length = value;
     }
 
     get students(){
         return this._students;
     }
-    set students(students) {
+    set students(value) {
+        if (!(value instanceof Array)){
+            throw TypeError("Students must be an array");
+        }
+        if (!value.every((student) => typeof student === 'string')) {
+            throw new TypeError('Students must be an array of strings');
+          }
         this._students = students;
     }
 }
