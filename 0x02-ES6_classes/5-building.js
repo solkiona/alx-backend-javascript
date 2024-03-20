327790b5491d8a210dd8b1a516249a46d78c7112
SOLKIONA <solkiona@gmail.com>
2024-03-20 12:05:23 +0100

    building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,20 @@
+export default class Building {
+    constructor(sqft) {
+        this.sqft = sqft;
+        if (this.constructor !== Building) {
+            if (typeof this.evacuationWarningMessage !== 'function') {
+                throw new TypeError('Class extending Building must override evacuationWarningMessage');
+            }
+        }
+    }
+
+    get sqft() {
+        return this._sqft;
+    }
+    set sqft(value) {
+        if (typeof value!== 'number') {
+            throw new TypeError('sqft must be a number');
+        }
+        this._sqft = value;
+    }
+}
